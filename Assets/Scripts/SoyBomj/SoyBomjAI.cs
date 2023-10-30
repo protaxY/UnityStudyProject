bfb0972191c4e71f7a40c5b269c01126d0a6c2f4
protaxY <62854654+protaxY@users.noreply.github.com>
2023-10-31 01:41:58 +0300

    dummy patrolling added

--- Assets/Scripts/SoyBomj/SoyBomjAI.cs
+++ Assets/Scripts/SoyBomj/SoyBomjAI.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class SoyBomjAI : MonoBehaviour
+public class SoyBomjAi : MonoBehaviour
 {
+    private NavMeshAgent agent;
+
+    //Animation
+    private Animator animator;
+
+    [SerializeField] public Transform player;
+
+    [SerializeField] public LayerMask whatIsWalkable, whatIsPlayer;
+
+    //Patroling
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+    [SerializeField] public float walkPointRange;
+
+    //Attacking
+    [SerializeField] public float timeBetweenAttacks;
+    private bool alreadyAttacked;
+
+    // Flipping
+    
+    //States
+    public float sightRange, attackRange;
+    [SerializeField] public bool playerInSightRange, playerInAttackRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        //Debug.Log(animator.parameters);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Check for sight and attack range
+        //playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        //playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        //if (!playerInSightRange && !playerInAttackRange) Patroling();
+        //if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        //if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        Patroling();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+            if (!animator.GetBool("IsWalking"))
+                animator.SetBool("IsWalking", true);
+
+        }
+
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+            if (animator.GetBool("IsWalking"))
+                animator.SetBool("IsWalking", false);
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsWalkable))
+            walkPointSet = true;
     }
 }
